57bc77cce7422ad55f0cea79d8e6e7e6b8af87a8
Marysah <Marysah2@gmail.com>
2025-10-27 15:12:12 +0300

    solving the props tests

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,31 @@
 import React from "react";
+import user from "../data/user";
+import Links from "./Links"
 
-function About() {
+
+function About({bio, links}) {
+  const {github, linkedin} = links;
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      <Bio bio={bio}/>
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      <Links github={github} linkedin={linkedin}/>
     </div>
   );
 }
+ function Bio({bio}) {
+   if(bio && bio.length > 0) {
+    return <p>{bio}</p>
+  } else {
+    return null;
+  }
+
+}
+
+
+ 
+
+
 
 export default About;
---
+++ src/components/Links.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+function Links({ github, linkedin }) {
+
+    return (
+<div>
+    <div>Links</div>
+    <a href={github}>{github}</a>
+    <a href={linkedin}>{linkedin}</a>
+</div>
+
+    )
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+export default Links;
